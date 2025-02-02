3870e99d492ef1c55c76acd03b91a2acaaa0efcd
Adrian Estrada Jr <adrianestrada497@gmail.com>
2025-02-02 15:21:13 -0600

    Module $ Completed Baseline

---
+++ app_server/models/seed.js
@@ -0,0 +1,20 @@
+const Mongoose = require('./db');
+const Trip = require('./travlr');
+
+// read seed data from json file
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+//delete any existing records, then insert seed data
+
+const seedDB = async () => {
+    await Trip.deleteMany({});
+    await Trip.insertMany(trips);
+};
+
+
+//close the MongoDb connection and exit
+seedDB().then(async () => {
+    await Mongoose.connection.close();
+    process.exit(0);
+});
